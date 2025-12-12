e94fd4af9cc4cc2438ff41eb5738ad1ead4f20c4
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-12-11 21:06:06 -0300

    implementado o spring security

--- src/main/java/vendasApi/config/SecurityConfig.java
+++ src/main/java/vendasApi/config/SecurityConfig.java
@@ -7,8 +7,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+import vendasApi.security.jwt.JwtAuthFilter;
+import vendasApi.security.jwt.JwtService;
 import vendasApi.service.impl.UsuarioServiceImpl;
 
 @EnableWebSecurity
@@ -17,10 +22,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UsuarioServiceImpl usuarioService;
+    @Autowired
+    JwtService jwtService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter() {
+        return new JwtAuthFilter(jwtService, usuarioService);
     }
 
     @Override
@@ -43,6 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
--- src/main/java/vendasApi/security/jwt/JwtService.java
+++ src/main/java/vendasApi/security/jwt/JwtService.java
@@ -1,4 +1,4 @@
-package vendasApi;
+package vendasApi.security.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
