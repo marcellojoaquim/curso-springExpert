05ae7e2535d40e602a8fbfa4bdeb52a3cc0e0977
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-12-22 11:28:29 -0300

    criacao de profiles para geracao de diferentes empacotamentos jar e war

--- src/main/java/vendasApi/VendasApplication.java
+++ src/main/java/vendasApi/VendasApplication.java
@@ -3,9 +3,10 @@ package vendasApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
